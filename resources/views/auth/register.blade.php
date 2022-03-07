f32e9745c1fe9d35e5b9d6a0625eb573526718da
la#0001 <76826837+3UR@users.noreply.github.com>
2022-03-08 00:38:32 +1000

    working register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,87 +3,135 @@
 @section('content')
 <div id="Registration">
     <div id="ctl00_cphRoblox_upAccountRegistration">
-        <h2>Sign Up and Play</h2>
-        <h3>Step 1 of 2: Create Account</h3>
-        <div id="EnterAgeGroup">
-            <fieldset title="Provide your age-group">
-                <legend>Provide your age-group</legend>
-                <div class="Suggestion">
-                    This will help us to customize your experience.  Users under 13 years will only be shown pre-approved images.
+    @if (config('app.registration_enabled'))
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+
+            <h2>Sign Up and Play</h2>
+            <h3>Step 1 of 2: Create Account</h3>
+            
+            <div id="EnterUsername">
+                <fieldset title="Choose a name for your {{ config('app.name') }} character">
+                    <legend>Choose a name for your {{ config('app.name') }} character</legend>
+                    <div class="Suggestion">
+                        Use 3-20 alphanumeric characters: A-Z, a-z, 0-9, no spaces
+                    </div>
+                    
+                    <div class="Validators">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    
+                    <div class="UsernameRow">
+                        <label for="username" id="UserNameLabel" class="Label">Character Name:</label>&nbsp;<input id="username" type="text" class="TextBox @error('username') is-invalid @enderror" name="username" placeholder="{{ ('Username') }}" required autocomplete="username" tabindex="1" autofocus>
+                        
+                        @error('username')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </fieldset>
+            </div>
+
+            @if (config('app.invite_keys_required'))
+                <div id="EnterInviteKey">
+                    <fieldset title="{{ config('app.name') }} requires a invite key">
+                        <legend>{{ config('app.name') }} requires a invite key</legend>
+                        <div class="Suggestion">
+                            You have to be invited.
+                        </div>
+                        
+                        <div class="Validators">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        
+                        <div class="KeyRow">
+                            <label for="invite_key" id="UserNameLabel" class="Label">Invite Key:</label>&nbsp;
+                            <input id="invite_key" type="text" class="TextBox @error('invite_key') is-invalid @enderror" name="invite_key" placeholder="{{ ('Invite Key') }}" required tabindex="1">
+                            
+                            @error('invite_key')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </fieldset>
                 </div>
-                <div class="AgeGroupRow">
-                    <span id="ctl00_cphRoblox_rblAgeGroup"><input id="ctl00_cphRoblox_rblAgeGroup_0" type="radio" name="ctl00$cphRoblox$rblAgeGroup" value="1" checked="checked" tabindex="5"><label for="ctl00_cphRoblox_rblAgeGroup_0">Under 13 years</label><br><input id="ctl00_cphRoblox_rblAgeGroup_1" type="radio" name="ctl00$cphRoblox$rblAgeGroup" value="2" onclick="javascript:setTimeout('__doPostBack(\'ctl00$cphRoblox$rblAgeGroup$1\',\'\')', 0)" tabindex="5"><label for="ctl00_cphRoblox_rblAgeGroup_1">13 years or older</label></span>
-                </div>
-            </fieldset>
-        </div>
-        <div id="EnterUsername">
-            <fieldset title="Choose a name for your ROBLOX character">
-                <legend>Choose a name for your ROBLOX character</legend>
-                <div class="Suggestion">
-                    Use 3-20 alphanumeric characters: A-Z, a-z, 0-9, no spaces
-                </div>
-                <div class="Validators">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div class="UsernameRow">
-                    <label for="ctl00_cphRoblox_UserName" id="ctl00_cphRoblox_UserNameLabel" class="Label">Character Name:</label>&nbsp;<input name="ctl00$cphRoblox$UserName" type="text" id="ctl00_cphRoblox_UserName" tabindex="1" class="TextBox">
-                </div>
-            </fieldset>
-        </div>
-        <div id="EnterPassword">
-            <fieldset title="Choose your ROBLOX password">
-                <legend>Choose your ROBLOX password</legend>
-                <div class="Suggestion">
-                    4-10 characters, no spaces
-                </div>
-                <div class="Validators">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div class="PasswordRow">
-                    <label for="ctl00_cphRoblox_Password" id="ctl00_cphRoblox_LabelPassword" class="Label">Password:</label>&nbsp;<input name="ctl00$cphRoblox$Password" type="password" id="ctl00_cphRoblox_Password" tabindex="2" class="TextBox">
-                </div>
-                <div class="ConfirmPasswordRow">
-                    <label for="ctl00_cphRoblox_TextBoxPasswordConfirm" id="ctl00_cphRoblox_LabelPasswordConfirm" class="Label">Confirm Password:</label>&nbsp;<input name="ctl00$cphRoblox$TextBoxPasswordConfirm" type="password" id="ctl00_cphRoblox_TextBoxPasswordConfirm" tabindex="3" class="TextBox">
-                </div>
-            </fieldset>
-        </div>
-        <div id="EnterChatMode">
-            <fieldset title="Choose your chat mode">
-                <legend>Choose your chat mode</legend>
-                <div class="Suggestion">
-                    All in-game chat is subject to profanity filtering and moderation.  For enhanced chat safety, choose SuperSafe Chat; only chat from pre-approved menus will be shown to you.
-                </div>
-                <div class="ChatModeRow">
-                    <span id="ctl00_cphRoblox_rblChatMode"><input id="ctl00_cphRoblox_rblChatMode_0" type="radio" name="ctl00$cphRoblox$rblChatMode" value="false" checked="checked" tabindex="6"><label for="ctl00_cphRoblox_rblChatMode_0">Safe Chat</label><br><input id="ctl00_cphRoblox_rblChatMode_1" type="radio" name="ctl00$cphRoblox$rblChatMode" value="true" tabindex="6"><label for="ctl00_cphRoblox_rblChatMode_1">SuperSafe Chat</label></span>
-                </div>
-            </fieldset>
-        </div>
-        <div id="EnterEmail">
-            <fieldset title="Provide your parent's email address">
-                <legend>Provide your parent's email address</legend>
-                <div class="Suggestion">
-                    This will allow you to recover a lost password
-                </div>
-                <div class="Validators">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div class="EmailRow">
-                    <label for="ctl00_cphRoblox_TextBoxEMail" id="ctl00_cphRoblox_LabelEmail" class="Label">Your Parent's Email:</label>&nbsp;<input name="ctl00$cphRoblox$TextBoxEMail" type="text" id="ctl00_cphRoblox_TextBoxEMail" tabindex="4" class="TextBox">
-                </div>
-            </fieldset>
-        </div>
-        <div class="Confirm">
-            <input type="submit" name="ctl00$cphRoblox$ButtonCreateAccount" value="Register" onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$cphRoblox$ButtonCreateAccount&quot;, &quot;&quot;, true, &quot;&quot;, &quot;&quot;, false, false))" id="ctl00_cphRoblox_ButtonCreateAccount" tabindex="5" class="BigButton">
-        </div>
+            @endif
+            
+            <div id="EnterPassword">
+                <fieldset title="Choose your {{ config('app.name') }} password">
+                    <legend>Choose your {{ config('app.name') }} password</legend>
+                    <div class="Suggestion">
+                        4-25 characters, no spaces
+                    </div>
+
+                    <div class="Validators">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    
+                    <div class="PasswordRow">
+                        <label for="password" id="LabelPassword" class="Label">Password:</label>&nbsp;
+                        <input id="password" type="password" class="TextBox @error('password') is-invalid @enderror" name="password" placeholder="Password" required tabindex="2">
+                    
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    
+                    <div class="ConfirmPasswordRow">
+                        <label for="password-confirm" id="LabelPasswordConfirm" class="Label">Confirm Password:</label>&nbsp;
+                        <input id="password-confirm" type="password" class="TextBox" name="password_confirmation" placeholder="Confirm Password" required tabindex="3">
+                    </div>
+                </fieldset>
+            </div>
+            
+            <div id="EnterEmail">
+                <fieldset title="Provide your parent's email address">
+                    <legend>Provide your parent's email address</legend>
+                    <div class="Suggestion">
+                        This will allow you to recover a lost password
+                    </div>
+                    
+                    <div class="Validators">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    
+                    <div class="EmailRow">
+                        <label for="email" class="Label" id="LabelEmail">Your Parent's Email:</label>&nbsp;<input id="email" type="email" class="TextBox @error('email') is-invalid @enderror" name="email" placeholder="{{ ('Email') }}" required autocomplete="email" tabindex="4">
+                    
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </fieldset>
+            </div>
+            
+            <div class="Confirm">
+                <input type="submit" name="ButtonCreateAccount" value="Register" onclick="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$cphRoblox$ButtonCreateAccount&quot;, &quot;&quot;, true, &quot;&quot;, &quot;&quot;, false, false))" id="ButtonCreateAccount" tabindex="5" class="BigButton">
+            </div>
+        </form>
+    @else
+        <h2>Registration closed</h2>
+        <p>Sorry, we're not taking new users at the moment. Check back in a bit.</p>
+    @endif
     </div>
 </div>
 @endsection
